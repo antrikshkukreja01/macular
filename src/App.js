592a9a7cb2c001700eb750ebfb24368ff73eb206
antrikshkukreja01 <antrikshkukreja01@gmail.com>
2023-05-23 14:20:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "../src/assets/css/Root.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
